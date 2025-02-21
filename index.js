25aa954d47db203cc97908b53eb7a4aab55720cf
Sahil Garg <129257534+Sahilivar@users.noreply.github.com>
2025-02-21 17:42:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config');
-const cors = require('cors');
+
 const app = express();
 app.use(bodyParser.json());
 
